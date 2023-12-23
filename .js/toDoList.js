cd36e17d257eb7b4e31940239008a2388b2fc304
amai-21 <andrew.y.mai@gmail.com>
2023-12-22 23:13:07 -0600

    I've made some progress on this. I guess I didn't need a PHP file to create a to-do list. Instead, I just need one file of each: html, css and js. I've started on a js file that adds tasks to a list courtesy of w3 schools. I'm still a newbie, but I'll become an expert programmer one day!

---
+++ .js/toDoList.js
@@ -0,0 +1,15 @@
+//Create a new list item when clicking on the "Add" button
+function newElement(){
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput");
+    var t = document.createTextNode(inputValue);
+
+    li.appendChild(t);
+    if (inputValue === ''){
+        alert("You must write something!");
+    } else{
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+}
+//https://www.w3schools.com/howto/howto_js_todolist.asp
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 This is my attempt at creating a To-Do-List! 
 Wish me luck!
+
+Create UserInput: (finished)
+-Textbox
+-Submit Button
+
+Saving and storing user inputs:
+-Store it
+-Display the task on string
+-Indicate if it's already finished or unfinished.
+    -Red text if it's not finished yet and a red x.
+    -If finished, move the finished task to another side of the screen
+        where the text is green, a check mark next ot the tasks and under a large "Finished" tag.
+        
+
